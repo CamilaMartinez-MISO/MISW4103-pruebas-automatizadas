0f50655934e1285a5eb56302a21bfe4c9fb93bdc
Camila Martinez <mc.martinezm12@uniandes.edu.co>
2024-05-05 12:09:47 -0500

    Feature 005 - case 001

---
+++ kraken/features/FU005_ESC001.feature
@@ -0,0 +1,32 @@
+Feature: Create a papge
+
+  @user2 @web
+  Scenario: As an admin user, I want to create and publish a Page to ensure that it is available and visible on the website.
+    # Given Section
+    Given I navigate to page "<baseURL>"
+    And I wait for 3 seconds
+    # When Section
+    When I sigin into Ghost "<email>" "<password>"
+    And I wait for 5 seconds
+    And I go to Pages
+    And I wait for 1 seconds
+    And I create a new page
+    And I wait for 1 seconds
+    And I set the page title
+    And I wait for 1 seconds
+    And I add content to the page
+    And I wait for 1 seconds
+    And I submit the page for publishing
+    And I wait for 1 seconds
+    And I want to publish page
+    And I wait for 1 seconds
+    And I confirm page publication
+    And I wait for 1 seconds
+    And I go back to the page editor page
+    And I wait for 1 seconds
+    And I go to Pages
+    And I wait for 1 seconds  
+    And I click on Published pages
+    And I wait for 1 seconds  
+    Then I verify that the page was created
+    
---
+++ kraken/features/web/step_definitions/FU005.js
@@ -0,0 +1,68 @@
+const { When, Then } = require('@cucumber/cucumber');
+const { faker } = require('@faker-js/faker');
+const assert = require('assert');
+
+const fakeTitle = faker.word.words({ count: { min: 2, max: 4 } });
+const fakeBody = faker.lorem.paragraphs({ min: 1, max: 5 });
+
+When('I go to Pages', async function () {
+    const element = await this.driver.$('a[href="#/pages/"]');
+    return await element.click();
+});
+
+When('I create a new page', async function () {
+    const element = await this.driver.$('a[href="#/editor/page/"]');
+    return await element.click();
+});
+
+When('I set the page title', async function () {
+    return await this.driver.$('textarea[placeholder="Page title"]').setValue(fakeTitle);
+});
+
+When('I add content to the page', async function () {
+    const articleSection = await this.driver.$('article.koenig-editor.w-100');
+    await articleSection.click();
+    return await articleSection.setValue(fakeBody);
+});
+
+When('I submit the page for publishing', async function () {
+    const element = await this.driver.$('.gh-publish-trigger');
+    return await element.click();
+});
+When('I want to publish page', async function () {
+    const element = await this.driver.$('.gh-publish-cta');
+    return await element.click();
+});
+
+When('I confirm page publication', async function () {
+    const element = await this.driver.$('.gh-btn.gh-btn-large.gh-btn-pulse.ember-view');
+    return await element.click();
+});
+
+When('I go back to the page editor page', async function () {
+    const element = await this.driver.$('button.gh-publish-back-button');
+    return await element.click();
+});
+
+When('I search for the page by title in the admin view', async function () {
+    let element = await this.driver.$('input.gh-contentfilter-input');
+    await element.setValue(fakeTitle);
+    return await element.keys('Enter');
+});
+
+When('I select the page to edit from search results', async function () {
+    await this.driver.$('div.gh-contentfilter').waitForDisplayed();
+    let element = await this.driver.$(`//h3[text()="${fakeTitle}"]`);
+    return await element.click();
+});
+
+When('I click on Published pages', async function () {
+    const element = await this.driver.$('a[title="Published"]');
+    return await element.click();
+});
+
+Then('I verify that the page was created', async function () {
+    const elements = await this.driver.$$('h3.gh-content-entry-title');
+    const elementWithFakeTitle = elements.find(async element => (await element.getText()) === fakeTitle);
+    assert(elementWithFakeTitle, `Page "${fakeTitle}" was not created`);
+});
