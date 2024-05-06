f56214bbd6f910eb09c486b15899b6991606bf80
CamilaMartinez-MISO <mc.martinezm12@uniandes.edu.co>
2024-05-05 19:10:24 -0500

    add feature 05 in tests

--- cypress/cypress/e2e/ALL_TESTS/allTests.cy.js
+++ cypress/cypress/e2e/ALL_TESTS/allTests.cy.js
@@ -4,6 +4,7 @@
 require('../FU002/FU002.cy')
 require('../FU003/FU003.cy')
 require('../FU004/FU004.cy')
+require('../FU005/FU005.cy')
 require('../FU006/FU006.cy')
 require('../FU007/FU007.cy')
 require('../FU008/FU008.cy')
