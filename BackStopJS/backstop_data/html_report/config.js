02d6f821745587dd50db8dcc8fd21a4b46050ca7
JSVargasQ <jsvargasq@hotmail.com>
2024-05-11 23:11:00 -0500

    Update v1 for BackstopJs

--- BackStopJS/backstop_data/html_report/config.js
+++ BackStopJS/backstop_data/html_report/config.js
@@ -1,5 +1,168 @@
 report({
   "testSuite": "BackstopJS",
-  "tests": [],
+  "tests": [
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\1_FU012_ESC001_1_0_document_0_default.png",
+        "test": "..\\bitmaps_test\\20240511-230923\\1_FU012_ESC001_1_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "1_FU012_ESC001_1_0_document_0_default.png",
+        "label": "FU012_ESC001_1",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "../Ghost_v3/cypress/cypress/screenshots/FU012_ESC001/cleanAndTypeDescription.png",
+        "referenceUrl": "FU012_ESC001/cleanAndTypeDescription.png",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 200,
+            "height": 60
+          },
+          "rawMisMatchPercentage": 20.75378787878788,
+          "misMatchPercentage": "20.75",
+          "analysisTime": 154
+        },
+        "diffImage": "..\\bitmaps_test\\20240511-230923\\failed_diff_1_FU012_ESC001_1_0_document_0_default.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\1_FU012_ESC001_2_0_document_0_default.png",
+        "test": "..\\bitmaps_test\\20240511-230923\\1_FU012_ESC001_2_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "1_FU012_ESC001_2_0_document_0_default.png",
+        "label": "FU012_ESC001_2",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "../Ghost_v3/cypress/cypress/screenshots/FU012_ESC001/clickOnExpandTitleAndDescriptionOptions.png",
+        "referenceUrl": "FU012_ESC001/clickOnExpandTitleAndDescriptionOptions.png",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 200,
+            "height": 60
+          },
+          "rawMisMatchPercentage": 19.119393939393937,
+          "misMatchPercentage": "19.12",
+          "analysisTime": 149
+        },
+        "diffImage": "..\\bitmaps_test\\20240511-230923\\failed_diff_1_FU012_ESC001_2_0_document_0_default.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\1_FU012_ESC001_3_0_document_0_default.png",
+        "test": "..\\bitmaps_test\\20240511-230923\\1_FU012_ESC001_3_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "1_FU012_ESC001_3_0_document_0_default.png",
+        "label": "FU012_ESC001_3",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "../Ghost_v3/cypress/cypress/screenshots/FU012_ESC001/clickOnGeneralSettings.png",
+        "referenceUrl": "FU012_ESC001/clickOnGeneralSettings.png",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 200,
+            "height": 60
+          },
+          "rawMisMatchPercentage": 19.363030303030303,
+          "misMatchPercentage": "19.36",
+          "analysisTime": 136
+        },
+        "diffImage": "..\\bitmaps_test\\20240511-230923\\failed_diff_1_FU012_ESC001_3_0_document_0_default.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\1_FU012_ESC001_4_0_document_0_default.png",
+        "test": "..\\bitmaps_test\\20240511-230923\\1_FU012_ESC001_4_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "1_FU012_ESC001_4_0_document_0_default.png",
+        "label": "FU012_ESC001_4",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "../Ghost_v3/cypress/cypress/screenshots/FU012_ESC001/clickOnSaveSettings.png",
+        "referenceUrl": "FU012_ESC001/clickOnSaveSettings.png",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 200,
+            "height": 60
+          },
+          "rawMisMatchPercentage": 21.186818181818182,
+          "misMatchPercentage": "21.19",
+          "analysisTime": 153
+        },
+        "diffImage": "..\\bitmaps_test\\20240511-230923\\failed_diff_1_FU012_ESC001_4_0_document_0_default.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\1_FU012_ESC001_5_0_document_0_default.png",
+        "test": "..\\bitmaps_test\\20240511-230923\\1_FU012_ESC001_5_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "1_FU012_ESC001_5_0_document_0_default.png",
+        "label": "FU012_ESC001_5",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "../Ghost_v3/cypress/cypress/screenshots/FU012_ESC001/signIn.png",
+        "referenceUrl": "FU012_ESC001/signIn.png",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": -200,
+            "height": -60
+          },
+          "rawMisMatchPercentage": 16.775000000000002,
+          "misMatchPercentage": "16.78",
+          "analysisTime": 162
+        },
+        "diffImage": "..\\bitmaps_test\\20240511-230923\\failed_diff_1_FU012_ESC001_5_0_document_0_default.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\1_FU012_ESC001_6_0_document_0_default.png",
+        "test": "..\\bitmaps_test\\20240511-230923\\1_FU012_ESC001_6_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "1_FU012_ESC001_6_0_document_0_default.png",
+        "label": "FU012_ESC001_6",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "../Ghost_v3/cypress/cypress/screenshots/FU012_ESC001/validate_description_site.png",
+        "referenceUrl": "FU012_ESC001/validate_description_site.png",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 200,
+            "height": 60
+          },
+          "rawMisMatchPercentage": 69.50681818181819,
+          "misMatchPercentage": "69.51",
+          "analysisTime": 164
+        },
+        "diffImage": "..\\bitmaps_test\\20240511-230923\\failed_diff_1_FU012_ESC001_6_0_document_0_default.png"
+      },
+      "status": "fail"
+    }
+  ],
   "id": "1"
 });
--- BackStopJS/configBackstop.js
+++ BackStopJS/configBackstop.js
@@ -1,8 +1,13 @@
 const fs = require('fs');
+const path = require('path');
 
 // Constants
 const PATH_GHOST_V5 = "../Ghost_v5/cypress/cypress/screenshots";
 const PATH_GHOST_V3 = "../Ghost_v3/cypress/cypress/screenshots";
+const PATH_BIT_MAPS_REFERENCE = "backstop_data/bitmaps_reference";
+
+const ID_CONFIG = "1";
+const LABEL_DEFAULT = "default";
 
 // =============================================
 // Methods for backstop
@@ -14,35 +19,79 @@ function createScenarios() {
     let scenarioFoldersV5 = getFoldersArray(PATH_GHOST_V5)
     let scenarioFoldersV3 = getFoldersArray(PATH_GHOST_V3)
 
-    console.log('carpetasV5', scenarioFoldersV5);
-    console.log('carpetasV3', scenarioFoldersV3);
+    let availableScenarios = scenarioFoldersV5.filter(element => scenarioFoldersV3.includes(element));
 
+    for (let i = 0; i < availableScenarios.length; i++) {
 
+        let scenarioId = availableScenarios[i];
+
+        let imagesV5 = getFilesInPath(PATH_GHOST_V5 + '/' + scenarioId);
+        let imagesV3 = getFilesInPath(PATH_GHOST_V3 + '/' + scenarioId);
+
+        let availableImages = imagesV5.filter(element => imagesV3.includes(element));
+        console.log('availableImages',availableImages);
+
+        for (let j = 0; j < availableImages.length; j++) {
+            let originalNameFile = availableImages[j];
+            let nameFile = `${ID_CONFIG}_${scenarioId}_${j+1}_0_document_0_${LABEL_DEFAULT}.png`;
+            console.log(nameFile);
+            copyAndPasteFile(`${PATH_GHOST_V5}/${scenarioId}/${originalNameFile}`, `${PATH_BIT_MAPS_REFERENCE}`, nameFile);
+            scenarios.push( createScenario(scenarioId, originalNameFile, j+1) );
+        }
+    }
 
     return scenarios;
 }
 
 function getFoldersArray(path) {
     try {
-        // Lee el contenido del directorio
         const folderContent = fs.readdirSync(path, { withFileTypes: true });
-
-        // Filtra solo las carpetas
         const folders = folderContent.filter(item => item.isDirectory()).map(item => item.name);
-
         return folders;
     } catch (error) {
-        console.error('Error al obtener las carpetas:', error);
+        console.error('Error in get folders:', error);
         return [];
     }
 }
 
+function getFilesInPath(path) {
+    try {
+        const folderContent = fs.readdirSync(path, { withFileTypes: true });
+        const files = folderContent.filter(item => item.isFile()).map(item => item.name);
+        return files;
+    } catch (error) {
+        console.error('Error in get files:', error);
+        return [];
+    }
+}
 
-function createScenario(scenarioId, imageName) {
+function copyAndPasteFile(origin, destination, newNameFile) {
+    try {
+        // Lee el contenido del archivo de origen
+        const fileContent = fs.readFileSync(origin);
+
+        // Crea la ruta completa para el destino con el nuevo nombre
+        const pathDestination = path.join(destination, newNameFile).toString();
+
+        // Escribe el contenido en el nuevo archivo
+        fs.writeFileSync(pathDestination, fileContent);
+
+        console.log(`Archivo copiado exitosamente a ${pathDestination}`);
+    } catch (error) {
+        console.error('Error al copiar el archivo:', error);
+    }
+}
+
+
+function createScenario(scenarioId, imageName, index) {
+
+    console.log('url',PATH_GHOST_V3 + "/" + scenarioId +"/" + imageName,)
+    console.log("referenceUrl", scenarioId + "/" + imageName);
+
     return {
-        "label": scenarioId,
+        "label": scenarioId + '_' + index,
+        "url": PATH_GHOST_V3 + "/" + scenarioId +"/" + imageName,
         "referenceUrl": scenarioId + "/" + imageName,
-        "url": PATH_GHOST_V5 + "/" + scenarioId +"/validate_description_site.png",
         "readyEvent": "",
         "readySelector": "",
         "delay": 0,
@@ -61,10 +110,10 @@ function createScenario(scenarioId, imageName) {
 
 function createConfigWithScenarios( scenarios ) {
     return {
-        "id": "1",
+        "id": ID_CONFIG,
         "viewports": [
             {
-                "label": "default",
+                "label": LABEL_DEFAULT,
                 "width": 800,
                 "height": 600
             }
@@ -73,7 +122,7 @@ function createConfigWithScenarios( scenarios ) {
         "onReadyScript": "puppet/onReady.js",
         "scenarios": scenarios,
         "paths": {
-            "bitmaps_reference": PATH_GHOST_V5,
+            "bitmaps_reference": "backstop_data/bitmaps_reference",
             "bitmaps_test": "backstop_data/bitmaps_test",
             "engine_scripts": "backstop_data/engine_scripts",
             "html_report": "backstop_data/html_report",
@@ -94,8 +143,5 @@ function createConfigWithScenarios( scenarios ) {
 // Create dynamic config
 const scenarios = createScenarios();
 const backstopConfig = createConfigWithScenarios(scenarios);
-
-// Execute backstop
-//backstop('test', backstopConfig);
 
 module.exports = backstopConfig;
