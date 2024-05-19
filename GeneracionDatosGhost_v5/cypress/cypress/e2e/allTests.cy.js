a8774c959a028571669e6002395af44ed4385701
JSVargasQ <jsvargasq@hotmail.com>
2024-05-19 18:00:33 -0500

    Add test for run all tests in Cypress

---
+++ GeneracionDatosGhost_v5/cypress/cypress/e2e/allTests.cy.js
@@ -0,0 +1,12 @@
+/**
+ * RUN ALL FEATURES AT ONCE
+ */
+require('./FU002.cy')
+require('./FU003.cy')
+require('./FU006.cy')
+require('./FU009.cy')
+require('./FU012.cy')
+require('./FU016.cy')
+require('./FU021.cy')
+require('./FU023.cy')
+require('./FU024.cy')
