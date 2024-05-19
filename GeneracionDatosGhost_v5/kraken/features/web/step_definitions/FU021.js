fabd413215487f8a611fa346e94df61082ca2fb8
CamilaMartinez-MISO <mc.martinezm12@uniandes.edu.co>
2024-05-19 16:01:22 -0500

    FU021 Kraken pseudo scenarios

--- GeneracionDatosGhost_v5/kraken/features/web/step_definitions/FU021.js
+++ GeneracionDatosGhost_v5/kraken/features/web/step_definitions/FU021.js
@@ -1,5 +1,8 @@
 const { When, Then } = require('@cucumber/cucumber');
 const assert = require('assert');
+const { faker } = require('@faker-js/faker');
+
+const fakeTagName = faker.word.words({ count: 1 })
 
 When('I click on Tags link', async function () {
     let element = await this.driver.$('a[href="#/tags/"]');
@@ -22,6 +25,12 @@ When(/^I enter the Tag Name (.*)$/, async function (name) {
     return await element.setValue(name)
 });
 
+When("I enter any value in Tag Name", async function () {
+    let element = await this.driver.$('input[name="name"]');
+    await element.click()
+    return await element.setValue(fakeTagName);
+});
+
 When(/^I enter the Tag Slug (.*)$/, async function (name) {
     let element = await this.driver.$('input[name="slug"]');
     await element.click()
@@ -34,6 +43,19 @@ When(/^I enter the Tag Description (.*)$/, async function (description) {
     return await element.setValue(description)
 });
 
+When("I enter a long value in Tag Description", async function () {
+    let element = await this.driver.$('textarea[name="description"]');
+    let description = faker.lorem.words(150);
+    await element.click()
+    return await element.setValue(description);
+});
+
+When("I enter a long value in Tag Slug", async function () {
+    let element = await this.driver.$('input[name="slug"]');
+    let slug = faker.lorem.words(200);
+    await element.click()
+    return await element.setValue(slug);
+});
 
 When('I click on Save Tag button', async function () {
     let element = await this.driver.$('button[class="gh-btn gh-btn-primary gh-btn-icon ember-view"]');
@@ -56,10 +78,31 @@ When(/^I enter the Meta title (.*)$/, async function (metaTitle) {
     return await element.setValue(metaTitle)
 });
 
+When("I enter a random Meta title", async function () {
+    let element = await this.driver.$('input[name="metaTitle"]');
+    let metaTitle = faker.lorem.words(20);
+    await element.click()
+    return await element.setValue(metaTitle);
+});
+
+When("I enter a random Meta description", async function () {
+    let element = await this.driver.$('textarea[name="metaDescription"]');
+    let metaDescription = faker.lorem.words(30);
+    await element.click()
+    return await element.setValue(metaDescription);
+});
+
 When(/^I enter the Canonical Link (.*)$/, async function (link) {
     let element = await this.driver.$('input[name="canonicalUrl"]');
     await element.click()
     return await element.setValue(link)
+});
+
+When("I enter a random Canonical Link", async function () {
+    let element = await this.driver.$('input[name="canonicalUrl"]');
+    let link = faker.internet.url();
+    await element.click()
+    return await element.setValue(link);
 });
 
 Then(/^I expect to see the Tag Name error message: (.*)$/, async function (message) {
@@ -90,4 +133,19 @@ Then(/^I expect to see the Canonical Link error message: (.*)$/, async function 
 Then(/^I expect to see the Canonical error message when edit: (.*)$/, async function (message) {
     let errorMessage  = await this.driver.$('article.gh-alert.gh-alert-red > div.gh-alert-content').getText();
     assert.equal(errorMessage, message)
+});
+
+Then('I check that my new tag was created', async function () {
+    const bodyText = await this.driver.$('body').getText();
+    assert(bodyText.includes(fakeTagName), `Expected tag with "${fakeTagName}" name wasn't found in page`);
+});
+
+Then('I expect to see the Tag Description error message', async function () {
+    let errorMessage = await this.driver.$('div.no-margin.form-group.error.ember-view > p.response').getText();
+    assert.equal(errorMessage, "Description cannot be longer than 500 characters.")
+});
+
+Then('I expect to see the Tag Slug error message', async function () {
+    let errorMessage = await this.driver.$('div.form-group.error.ember-view > p.response').getText();
+    assert.equal(errorMessage, "URL cannot be longer than 191 characters.")
 });
